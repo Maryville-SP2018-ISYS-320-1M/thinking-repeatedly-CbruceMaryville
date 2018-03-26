7b0ca0b1c1156f275cbb3dee7c7614b1a2d36a14
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:49:43 -0500

    Update P12_NumbersLeft.java

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -1,13 +1,42 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/25/2018
 */
-
-public class P12_NumbersLeft {
+public class NumbersLeft {
 
 	public static void main(String[] args) {
 
+		System.out.println(1);
+
+		for (int i = 1; i < 3; i++) {
+			System.out.print(2);
+		}
+
+		System.out.println();
+		
+		for (int j = 1; j < 4; j++) {
+			System.out.print(3);
+
+		}
+
+		System.out.println();
+
+		for (int j = 1; j < 5; j++) {
+			System.out.print(4);
+
+		}
+
+		System.out.println();
+
+		for (int j = 1; j < 6; j++) {
+			System.out.print(5);
+
+		}
+
+
+		
 	}
 
 }
+

63de933c37ba0f1233a77e8c2fa3d424b4b55cd7
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:49:19 -0500

    Update P11_TableMakerFlip.java

--- src/P11_TableMakerFlip.java
+++ src/P11_TableMakerFlip.java
@@ -1,18 +1,71 @@
+//I do not have any code like this from class. Im assuming it is from the session
+//I missed. Therefore I made the table myself.
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/25/2018
 */
-
-public class P11_TableMakerFlip {
+public class Tablemaker {
 
 	public static void main(String[] args) {
-		for( int rowNumber = 1; rowNumber <= 5; rowNumber++ ) {
-			for( int columnNumber = 1; columnNumber <= 10; columnNumber++ ) {
-				System.out.print( (rowNumber * columnNumber ) + "\t");
-			}
-			System.out.println();
-		}
+		PrintFives();
+		System.out.println();
+		PrintFours();
+		System.out.println();
+		PrintThrees();
+		System.out.println();
+		PrintTwos();
+		System.out.println();
+		PrintOnes();
 	}
 
+
+	
+	private static void PrintFives() {
+		for (int i = 1; i < 11; i++) {
+			System.out.print((i*5) + " ");
+		}
+
+	}
+
+	private static void PrintFours() {
+		for (int i=1; i<11; i ++) {
+			System.out.print(i*4 + " ");
+		}
+	}
+	
+	private static void PrintThrees() {
+		for (int i=1; i<11; i ++) {
+			System.out.print(i*3 + " ");
+		}
+	}
+	
+	private static void PrintTwos() {
+		for (int i=1; i<11; i ++) {
+			System.out.print(i*2 + " ");
+		}
+	}
+	
+	private static void PrintOnes() {
+		for (int i=1; i<11; i ++) {
+			System.out.print(i*1 + " ");
+		}
+	}
+	
+	
+	/*
+5 10 15 20 25 30 35 40 45 50 
+4 8 12 16 20 24 28 32 36 40 
+3 6 9 12 15 18 21 24 27 30 
+2 4 6 8 10 12 14 16 18 20 
+1 2 3 4 5 6 7 8 9 10 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
 }

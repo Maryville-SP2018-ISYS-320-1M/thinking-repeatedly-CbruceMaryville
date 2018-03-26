50c4068df7eb24ecd9ffc415d89ea566d850080d
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:50:21 -0500

    Update P1_Count5.java

--- src/P1_Count5.java
+++ src/P1_Count5.java
@@ -1,13 +1,21 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/25/2018
 */
-
 public class P1_Count5 {
     public static void main(String[] args) {
-        for( int i = /* FINISH ME */ ) {
-            System.out.println( /* FINISH ME */);
+        for( int i = 1; i<5; i++ ) {
+        	
+            System.out.println("5 times " + i + " = " + (5*i));
         }
     }
 }
+
+
+/*
+5 times 1 = 5
+5 times 2 = 10
+5 times 3 = 15
+5 times 4 = 20
+ */

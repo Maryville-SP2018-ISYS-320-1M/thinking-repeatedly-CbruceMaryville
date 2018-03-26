8ab7b7c7e59a5f7e159f770ded89344b966f2994
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:51:02 -0500

    Update P2_HowManyLines.java

--- src/P2_HowManyLines.java
+++ src/P2_HowManyLines.java
@@ -1,21 +1,28 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/25/2018
 */
-
-// 1. Your predicted output 
-
-
-
-public class P2_HowManyLines {
+public class Problem2 {
 
 	public static void main(String[] args) {
-
+		for( int i = 1; i <= 3; i++ ) 
+		    System.out.println( "How many lines" );
+		    System.out.println( "are printed?" );
 	}
 
 }
 
 
+//Prediction= It will print both phrases three times.
 
-// 3. Were you correct? Explain any differences and what you learned.
+//Output
+/*
+How many lines
+How many lines
+How many lines
+are printed?
+*/
+
+//I was correct for the first phrase not the second. 
+//Now I notice that the for has no {} so it just takes the first statement below and o

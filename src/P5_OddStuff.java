0c666e343dddcc065079f8d5e2730c91f67c650a
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:51:48 -0500

    Update P5_OddStuff.java

--- src/P5_OddStuff.java
+++ src/P5_OddStuff.java
@@ -1,27 +1,21 @@
+
 /*
   	ISYS 320
   	Name(s):Clayton Bruce
   	Date: 3/25/2018
 */
-public class P8_Launch {
+
+public class P5_OddStuff {
     public static void main( String[] args ) {
-        System.out.println("T-minus ");
-        for( int i = 5; i >= 1; i-- ) {
-            System.out.print( i + ", " );
+        int number = 4;
+        for( int count = 1; count <= number; count++ ) {
+            System.out.println( number );
+            number = number / 2;
         }
-        System.out.println("Blastoff!");
     }
 }
 
 /*
-	Prediction: A countdown from 5 using a for loop then when finished will say blastoff
-	
-	Conclusion: I was exactly correct 
-	
-	
-	
-	Output-
-	T-minus 
-5, 4, 3, 2, 1, Blastoff!
-
+	Prediction: will print a 4 and then a 2
+	My prediction was indeed correct this time!
 */

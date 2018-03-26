786a7edf6f10a76c32f2b8d8f27b03083651de8d
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 20:52:12 -0500

    Update P8_Launch.java

--- src/P8_Launch.java
+++ src/P8_Launch.java
@@ -1,21 +1,27 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 3/25/2018
 */
-
-// 1. Your predicted output 
-
-
-
 public class P8_Launch {
-
-	public static void main(String[] args) {
-
-	}
-
+    public static void main( String[] args ) {
+        System.out.println("T-minus ");
+        for( int i = 5; i >= 1; i-- ) {
+            System.out.print( i + ", " );
+        }
+        System.out.println("Blastoff!");
+    }
 }
 
+/*
+	Prediction: A countdown from 5 using a for loop then when finished will say blastoff
+	
+	Conclusion: I was exactly correct 
+	
+	
+	
+	Output-
+	T-minus 
+5, 4, 3, 2, 1, Blastoff!
 
-
-// 3. Were you correct? Explain any differences and what you learned.
+*/
